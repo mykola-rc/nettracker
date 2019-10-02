977a1dbecf09feb1898c749345f439e9e162d28f
Abbara <nick.burinok@me.com>
2019-10-01 20:50:08 -0400

    Initial Commit

---
+++ Net Worth Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Net Worth Tracker
+//
+//  Created by Mykola Burynok on 2019-10-01.
+//  Copyright © 2019 Mykola Burynok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Net Worth TrackerTests/Net_Worth_TrackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Net_Worth_TrackerTests.swift
+//  Net Worth TrackerTests
+//
+//  Created by Mykola Burynok on 2019-10-01.
+//  Copyright © 2019 Mykola Burynok. All rights reserved.
+//
+
+import XCTest
+@testable import Net_Worth_Tracker
+
+class Net_Worth_TrackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
